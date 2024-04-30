11b4e3786ef07d88a423deb36f10b6be82c165bf
Amira Ismail <amiraismail373@gmail.com>
2024-04-30 14:38:23 +0300

    Update IR

--- Core/Inc/IR_config.h
+++ Core/Inc/IR_config.h
@@ -9,9 +9,13 @@
 #define INC_IR_CONFIG_H_
 
 
-#define IR_PORT GPIOA
-#define IR_PIN_1 GPIO_PIN_0
-#define IR_PIN_2 GPIO_PIN_1
-#define IR_PIN_3 GPIO_PIN_2
-#define IR_PIN_4 GPIO_PIN_3
+#define IR1_PORT GPIOB
+#define IR2_PORT GPIOA
+#define IR3_PORT GPIOB
+#define IR4_PORT GPIOB
+
+#define IR_PIN_1 GPIO_PIN_14
+#define IR_PIN_2 GPIO_PIN_15
+#define IR_PIN_3 GPIO_PIN_3
+#define IR_PIN_4 GPIO_PIN_4
 #endif /* INC_IR_CONFIG_H_ */
